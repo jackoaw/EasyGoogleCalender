8bea3c72dc8619f4ebdf0d0101e88ee0e52bf52f
jackoaw <jackoaw@vt.edu>
2016-08-18 16:04:47 -0400

    added ROUGH support for basic dates and times

--- add_entries.js
+++ add_entries.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 		var lines = String(text).split("\n")
 		for (var i = 0; i < lines.length; i++) 
 		{
-			var parts = lines[i].split(":");
+			var parts = lines[i].split("=");
 			var date = parts[0];
 			var timeDate = dt(date.split(" "));
 			var DescriptLocation = parts[1].split("AT");
@@ -26,17 +26,38 @@ $(document).ready(function(){
 	// or for those more fun, downtown
 	function dt(rawTime)
 	{
+		// To and from entries expected in this array. On the same day for now.
+		// TODO: Expanded capabilities for events that extend over multiple days
+		var dates = [];
 		var date = new Date();
 		for (var i = 0; i < rawTime.length; i++) {
 			// Time
 			if(rawTime[i].split(":").length > 0)
 			{
-				
+				// if a date is not set, assume today
+				var time = rawTime[i].replace(" ","");
+				var timeToFrom = time.split("-");
+				// There is a speficied end time
+				if(timeToFrom.length > 0) 
+				{
+					date.setTime(timeToFrom[0]);
+					dates.push(date);
+					var date2 = jQuery.extend(true, {}, date);
+					dates.push(date2.setTime(timeToFrom[1]));
+				}
+				else
+				{
+					date.setTime(timeToFrom[0]);
+					dates.push(date);
+					var date2 = jQuery.extend(true, {}, date);
+					// assume default time of one hour
+					dates.push(date2.setTime(date.getTime()+1);
+				}
 			}
 			// Date
 			else if(rawTime[i].split("/").length > 0)
 			{
-
+				date.setDay(rawTime[i]);
 			}
 			// Day of the week, assuming
 			// Automatically determine the date based on a given day, chose the nearest one.
@@ -49,26 +70,28 @@ $(document).ready(function(){
 				{
 					curDay = 1;
 				}
-				if(rawTime[i].toLowerCase() == "tuesday")
+				else if(rawTime[i].toLowerCase() == "tuesday")
 				{
 					curDay = 2;
 				}
-				if(rawTime[i].toLowerCase() == "wednesday")
+				else if(rawTime[i].toLowerCase() == "wednesday")
 				{
 					curDay = 3;
 				}
-				if(rawTime[i].toLowerCase() == "thursday")
+				else if(rawTime[i].toLowerCase() == "thursday")
 				{
 					curDay = 4;
 				}
-				if(rawTime[i].toLowerCase() == "friday")
+				else if(rawTime[i].toLowerCase() == "friday")
 				{
 					curDay = 5;
 				}
-				if(rawTime[i].toLowerCase() == "saturday")
+				else if(rawTime[i].toLowerCase() == "saturday")
 				{
 					curDay = 6;
 				}
+				else
+					continue;
 				if (day < curDay)
 					day += 7
 				var daysNeeded = day - curDay;
@@ -85,7 +108,7 @@ $(document).ready(function(){
 				}
 			}
 		}
-		return date;
+		return dates;
 	}
 
 	function findNextDay(dayOfWeek)
