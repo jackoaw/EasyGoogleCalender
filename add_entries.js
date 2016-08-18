489a8a74571d14feb13aa4342b91dd20d5832f85
jackoaw <jackoaw@vt.edu>
2016-08-18 15:32:48 -0400

    Support for only days specified

--- add_entries.js
+++ add_entries.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	function parseText(text)
 	{
 		var e = {}
-		var lines = text.split("\n")
+		var lines = String(text).split("\n")
 		for (var i = 0; i < lines.length; i++) 
 		{
 			var parts = lines[i].split(":");
@@ -26,11 +26,12 @@ $(document).ready(function(){
 	// or for those more fun, downtown
 	function dt(rawTime)
 	{
+		var date = new Date();
 		for (var i = 0; i < rawTime.length; i++) {
 			// Time
 			if(rawTime[i].split(":").length > 0)
 			{
-
+				
 			}
 			// Date
 			else if(rawTime[i].split("/").length > 0)
@@ -38,38 +39,53 @@ $(document).ready(function(){
 
 			}
 			// Day of the week, assuming
+			// Automatically determine the date based on a given day, chose the nearest one.
 			else
 			{
+				// Sunday by default
+				var curDay = 0
+				var day = date.getDay();
 				if (rawTime[i].toLowerCase() == "monday")
 				{
-
+					curDay = 1;
 				}
 				if(rawTime[i].toLowerCase() == "tuesday")
 				{
-
+					curDay = 2;
 				}
 				if(rawTime[i].toLowerCase() == "wednesday")
 				{
-
+					curDay = 3;
 				}
 				if(rawTime[i].toLowerCase() == "thursday")
 				{
-
+					curDay = 4;
 				}
 				if(rawTime[i].toLowerCase() == "friday")
 				{
-
+					curDay = 5;
 				}
 				if(rawTime[i].toLowerCase() == "saturday")
 				{
-
+					curDay = 6;
 				}
-				if(rawTime[i].toLowerCase() == "sunday")
+				if (day < curDay)
+					day += 7
+				var daysNeeded = day - curDay;
+				for (var i = 0; i < daysNeeded; i++) 
 				{
-
+					try
+					{
+						date.setDay(date.getDay()+1)
+					}
+					catch(Exception e)
+					{
+						// Could not set date for some reason
+					}
 				}
 			}
 		}
+		return date;
 	}
 
 	function findNextDay(dayOfWeek)
